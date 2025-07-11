d488044521683c2b082bc7ab3bb880205d35ec49
SeongHuiJung <tjdgml150237@naver.com>
2025-07-11 11:33:31 +0900

    [Add] MovieTableView 파일 생성

---
+++ SeSACWeek3/MovieTableView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieTableView.swift
+//  SeSACWeek3
+//
+//  Created by 정성희 on 7/11/25.
+//
+
+import UIKit
+
+class MovieTableView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+ㅇㅇ    }
+    */
+
+}
