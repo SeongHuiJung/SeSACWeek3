c787b5592702d6eaddf5e6a8262712d3a3ea13ef
SeongHuiJung <tjdgml150237@naver.com>
2025-07-11 11:06:15 +0900

    Initial Commit

---
+++ SeSACWeek3/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SeSACWeek3
+//
+//  Created by 정성희 on 7/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("1")
+        print("2")
+        print("3")
+    }
+}
